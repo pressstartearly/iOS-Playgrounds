0865336a317b0c4b325135f9d159abf3aee03690
Caleb Chapman <caleb@dedicatedmc.io>
2021-01-28 16:09:01 -0500

    Completed Homework1

--- Homework1.playground/Contents.swift
+++ Homework1.playground/Contents.swift
@@ -1,6 +1,7 @@
 // Created by: Caleb Chapman
 // Class: CSC308
 // Date: Jan 28th, 2021
+// Project Name: Homework1
 
 var products = ["Icecream Sunday":("Desert", 5.99), "Steak Dinner":("Entree", 40.00), "Coke":("Drink", 2.99)]
 
@@ -15,7 +16,7 @@ func lowestPrice(products: [String:(category: String, price: Double)]) -> (name:
             lowestPricedItem = (product.key, product.value.price)
         }
         // else lets check if it's a lower price. If it is lower then we shall replace the lowest priced item with the name and cost of the current item.
-        else if lowestPricedItem.price < product.value.price {
+        else if lowestPricedItem.price > product.value.price {
             lowestPricedItem = (product.key, product.value.price)
         }
     }
@@ -33,22 +34,28 @@ func highestPrice(products: [String:(category: String, price: Double)]) -> (name
             highestPricedItem = (product.key, product.value.price)
         }
         // else lets check if it's a higher price. If it is higher then we shall replace the highest priced item with the name and cost of the current item.
-        else if highestPricedItem.price > product.value.price {
+        else if highestPricedItem.price < product.value.price {
             highestPricedItem = (product.key, product.value.price)
         }
     }
     return highestPricedItem
 }
 
-func price(products: [String:(category: String, price: Double)], task: Int) -> ([String:(category: String, price: Double)])->(name: String, price: Double)
+// Runs the selected function. 0 for lowest price and anything else for highest price
+func price(products: [String:(category: String, price: Double)], task: Int)->(name: String, price: Double)
 {
     switch task {
     case 0:
-        return lowestPrice
+        return lowestPrice(products: products)
     default:
-        return highestPrice
+        return highestPrice(products: products)
     }
 }
 
+// Testing lowest price funtion
+print("Lowest Priced Item:")
 print(price(products: products, task: 0))
+
+// Testing highest price function
+print("\nHighest Priced Item:")
 print(price(products: products, task: 1))
