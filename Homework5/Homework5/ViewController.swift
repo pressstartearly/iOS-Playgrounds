22ce3f4c1c85f6354af8e9f1a386c6ccc1235552
Caleb <57644769+pressstartearly@users.noreply.github.com>
2021-03-03 13:18:53 -0500

    Done except for comments!

--- Homework5/Homework5/ViewController.swift
+++ Homework5/Homework5/ViewController.swift
@@ -50,12 +50,27 @@ class ViewController: UIViewController {
         winningsLabel.text = "Total Winnings: $\(cashMoney)"
         
         if totalClicks == 2 {
+            let alertController = UIAlertController(title: "Lucky You!", message: "You won $\(cashMoney) cash money!", preferredStyle: UIAlertController.Style.alert)
             
+            let exitAction = UIAlertAction(title: "Exit", style: UIAlertAction.Style.destructive, handler: {(alertAction: UIAlertAction) in exit(0)})
+            
+            let playAgainAction = UIAlertAction(title: "Play Again!", style: UIAlertAction.Style.default, handler: {(alertAction: UIAlertAction) in self.resetGame()})
+            
+            alertController.addAction(exitAction)
+            alertController.addAction(playAgainAction)
+            
+            present(alertController, animated: true, completion: nil)
         }
     }
     
     @IBAction func howTo(_ sender: UIButton) {
-        let alertController = UIAlertController(title:"How To Play", message "Click a", UIAlertAction.Style.alert)
+        let alertController = UIAlertController(title: "How To Play", message: "Click a dollar sign to get that cash money! You only get two chances to pick a lucky one!", preferredStyle: UIAlertController.Style.alert)
+        
+        let defaultAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
