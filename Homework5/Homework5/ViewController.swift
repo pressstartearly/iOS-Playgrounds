e1f92ae50ef35fe0a4e28b36ca02d61394367ab9
Caleb <57644769+pressstartearly@users.noreply.github.com>
2021-03-03 12:21:39 -0500

    Removes image on click

--- Homework5/Homework5/ViewController.swift
+++ Homework5/Homework5/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var scratchButton: [UIButton]!
+    
+    @IBAction func doScratchOff(_ sender: UIButton) {
+        let button = scratchButton.first(where: { $0.currentTitle == sender.currentTitle})
+        button?.setImage(nil, for: UIControl.State.normal)
+    }
+    
+    @IBAction func howTo(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+            
 
 }
 
